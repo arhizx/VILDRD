cf29f79f06889e4c428e9e68f5f14ecd89e7837b
Misha <mikhailrumov@gmail.com>
2021-05-09 23:23:22 +0300

    ---

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,10 @@ import "./About.scss";
 const About = () => {
   return (
     <div className="container">
-      <div className="about">Something unique made of stone//wood//metal</div>
+      <div className="about">
+        <p>Something unique made of stone//wood//metal </p>
+        <p>Material selection//sketch design//expression</p>
+      </div>
     </div>
   );
 };
